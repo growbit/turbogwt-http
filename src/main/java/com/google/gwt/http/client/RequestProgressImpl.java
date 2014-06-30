605d9b54c4c53d9cf075c2286e0c4c07c7ee8afb
Danilo Reinert <daniloreinert@gmail.com>
2014-06-30 17:47:06 -0300

    Some reorganization on gwt http classes.

--- src/main/java/com/google/gwt/http/client/RequestProgressImpl.java
+++ src/main/java/com/google/gwt/http/client/RequestProgressImpl.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.google.gwt.http.client;
 
 import com.google.gwt.xhr.client.XhrRequestProgress;
